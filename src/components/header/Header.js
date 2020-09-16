95992677ce2ba60177fc0d10416346c6d855f836
zzzfisherzzz <zzzfisherzzz@gmail.com>
2020-09-16 03:50:07 +0300

    Finish base table logic

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,23 +1,30 @@
-import { ExcelComponent } from '@core/ExcelComponent'
+import {ExcelComponent} from '@core/ExcelComponent'
 
 export class Header extends ExcelComponent {
-    static className = 'excel__header'
+  static className = 'excel__header'
 
-    toHTML() {
-        return `
-        <input type="text" class="input" value="Новая таблица">
+  constructor($root, options) {
+    super($root, {
+      name: 'Header',
+      ...options,
+    })
+  }
 
-        <div>
+  toHTML() {
+    return `
+      <input type="text" class="input" value="Новая таблица" />
 
-            <div class="button">
-                <i class="material-icons">delete</i>
-            </div>
+      <div>
 
-            <div class="button">
-                <i class="material-icons">exit_to_app</i>
-            </div>
-
+        <div class="button">
+          <i class="material-icons">delete</i>
         </div>
-        `
-    }
+
+        <div class="button">
+          <i class="material-icons">exit_to_app</i>
+        </div>
+
+      </div>
+    `
+  }
 }
